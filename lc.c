cae9f7ba53346f63c198d470282853e27a2f8f9c
lichao <jiexuelc@outlook.com>
2018-11-05 16:08:29 +0800

    test file

---
+++ lc.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: lc.c
+	> Author: jiexue
+	> Mail: jiexuelc@163.com
+	> Created Time: Mon 05 Nov 2018 03:54:22 PM CST
+	> Description:  
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    int j = 1;
+    int k = 2;
+
+    return 0;
+}
